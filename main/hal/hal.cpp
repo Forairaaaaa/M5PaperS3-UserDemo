0a2fb30a37383e5c25072fba25408a768fa7c12d
Forairaaaaa <applesyqd@outlook.com>
2025-07-09 11:55:21 +0800

    update workflow

--- main/hal/hal.cpp
+++ main/hal/hal.cpp
@@ -17,7 +17,7 @@
 #include <esp_vfs_fat.h>
 #include <esp_wifi.h>
 #include <nvs.h>
-#include "nvs_flash.h"
+#include <nvs_flash.h>
 #include <esp_netif.h>
 #include <esp_err.h>
 #include <algorithm>
